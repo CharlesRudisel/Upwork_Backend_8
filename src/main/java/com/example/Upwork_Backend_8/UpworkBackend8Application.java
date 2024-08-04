f44dfd495184c655b40924a8c128b56a2100ff1b
charles <charlesrudisel@gmail.com>
2024-08-04 13:44:08 -0400

    first commit

---
+++ src/main/java/com/example/Upwork_Backend_8/UpworkBackend8Application.java
@@ -0,0 +1,13 @@
+package com.example.Upwork_Backend_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpworkBackend8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpworkBackend8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Upwork_Backend_8/UpworkBackend8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Upwork_Backend_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpworkBackend8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
