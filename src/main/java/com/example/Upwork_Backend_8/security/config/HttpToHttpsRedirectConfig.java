1a28a6fbcfc7bc81cb13a20864fb0fe03f026183
charles <charlesrudisel@gmail.com>
2024-08-04 14:48:55 -0400

    added HttpToHttpsRedirectingConfig

---
+++ src/main/java/com/example/Upwork_Backend_8/security/config/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,27 @@
+package com.example.Upwork_Backend_8.security.config;
+
+
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpToHttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> server.addAdditionalTomcatConnectors(createStandardConnector());
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
